829988b9179756a8b058fb2979d61dbb6a914cc1
phoebedo <dophuongthao15599@gmail.com>
2022-01-21 22:19:05 -0500

    registration

--- market/models.py
+++ market/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     budget = db.Column(db.Integer(), nullable = False, default=1000)
     items = db.relationship("Item", backref="owned_user", lazy = True)
 
+    def __repr__(self) -> str:
+        return f"username: {self.username}"
 class Item(db.Model):
     id = db.Column(db.Integer(),primary_key=True)
     name = db.Column(db.String(length=30),nullable = False, unique= True)
--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,11 @@
 # from flask import Flask, render_template
 # app = Flask(__name__)
+from crypt import methods
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for, flash
+from market.models import Item, User
+from market.forms import RegisterForm
+from market import db
 @app.route("/")
 @app.route('/home')
 def homepage():
@@ -12,7 +15,23 @@ def homepage():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+@app.route('/register', methods=['GET','POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,email_address=form.email_address.data,password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for("market_page"))
+
+    if form.errors != {}:
+        for err in form.errors.values():
+            flash(f'ERROR: {err[0]}', category='danger')
+        
+    return render_template("register.html", form=form)
+
 # dynamic route
-@app.route("/about/<username>")
-def profile(username):
-    return f"this is the page of {username}"
+# @app.route("/about/<username>")
+# def profile(username):
+#     return f"this is the page of {username}"
